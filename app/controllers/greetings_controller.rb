46d8a487ea7b1de3ee9b2aecfac3ac4892292e92
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-06-06 17:58:45 -0300

    Fix rubocop errors

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -4,7 +4,7 @@ class GreetingsController < ApplicationController
     @random_greeting = @greetings[rand(@greetings.length)]
 
     respond_to do |format|
-      format.json { render :json => @random_greeting }
+      format.json { render json: @random_greeting }
     end
   end
 end
