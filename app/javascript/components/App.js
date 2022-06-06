e81fa4a325486035c5c920fa3d96ecbe664abe2e
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-06-06 18:31:38 -0300

    Add App react component

---
+++ app/javascript/components/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+class App extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        Hello
+      </React.Fragment>
+    );
+  }
+}
+
+export default App
