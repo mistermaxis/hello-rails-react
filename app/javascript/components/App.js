2bf684700b7e0415f60311d6d15a4685915685e6
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-06-07 17:00:56 -0300

    Create App and Index components

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,17 +1,22 @@
 import React from "react"
+import { Provider } from "react-redux";
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import store from "../redux/store";
 import Greeting from "./Greeting";
 import Index from "./Index";
+
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
-        <Link to="/greeting">Greeting</Link>
-        <Routes>
-          <Route path="/" element={<Index />}/>
-          <Route path="/greeting" element={<Greeting />} />
-        </Routes>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Link to="/greeting">Greeting</Link>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/greeting" element={<Greeting />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
