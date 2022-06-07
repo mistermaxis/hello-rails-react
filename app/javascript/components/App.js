3fca76a2f2901c3dd9f35226ec548881f663f0d9
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-06-06 22:22:28 -0300

    Create basic layout

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,12 +1,19 @@
 import React from "react"
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import Greeting from "./Greeting";
+import Index from "./Index";
 class App extends React.Component {
-  render () {
+  render() {
     return (
-      <React.Fragment>
-        Hello
-      </React.Fragment>
+      <BrowserRouter>
+        <Link to="/greeting">Greeting</Link>
+        <Routes>
+          <Route path="/" element={<Index />}/>
+          <Route path="/greeting" element={<Greeting />} />
+        </Routes>
+      </BrowserRouter>
     );
   }
 }
 
-export default App
+export default App;
